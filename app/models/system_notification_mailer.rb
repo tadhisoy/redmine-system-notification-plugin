abd45a0bad004e25a76c12114cdfb993fa35fbbc
Takashi Yoshida <yoshida@octopath.co.jp>
2014-03-17 22:30:10 +0900

    fix bug on mail footer.

--- app/models/system_notification_mailer.rb
+++ app/models/system_notification_mailer.rb
@@ -6,8 +6,8 @@ class SystemNotificationMailer < Mailer2
     @body = system_notification.body
     mail(:to => system_notification.users.collect(&:mail), :subject => system_notification.subject, :from => User.current.mail) do |format|
 #    mail(:to => system_notification.users.collect(&:mail), :subject => system_notification.subject) do |format|
-      format.text ## { render layout: "system_notification.text" }
-      format.html ## { render layout: "system_notification.html" }
+      format.text { render layout: "system_notification.text" }
+      format.html { render layout: "system_notification.html" }
     end
   end
 
